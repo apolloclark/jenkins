6f94b5bd6a2d3dadae0b0b451e015fc6293e1e08
Apollo Clark <apolloclark@gmail.com>
2016-10-21 16:27:59 -0400

    fixed bootstrap.sh, to be runnable by non-root users

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 # Update Aptitude
-apt-get update
+sudo apt-get update
+export DEBIAN_FRONTEND="noninteractive"
 
 
 
 # Install Jenkins
 # @see https://wiki.jenkins-ci.org/display/JENKINS/Installing+Jenkins+on+Ubuntu
 wget -q -O - http://pkg.jenkins-ci.org/debian-stable/jenkins-ci.org.key | \
-	apt-key add -
-sh -c 'echo "deb http://pkg.jenkins-ci.org/debian-stable binary/" > /etc/apt/sources.list.d/jenkins.list'
-apt-get -y update
-apt-get install -y openjdk-7-jre-headless daemon git-core jenkins
+	sudo apt-key add -
+sudo sh -c 'echo "deb http://pkg.jenkins-ci.org/debian-stable binary/" > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get -y update
+sudo apt-get install -y openjdk-8-jre-headless daemon git-core jenkins
 
 # hackish way to install a specific version of Jenkins
 # dpkg --install /vagrant/jenkins_1.642_all.deb
-service jenkins restart
+sudo service jenkins restart
 
 
 
@@ -23,29 +24,29 @@ service jenkins restart
 # @see http://updates.jenkins-ci.org/download/plugins/
 # @see /var/lib/jenkins/plugins/
 echo "INFO: Installing Jenkins plugins..."
-mkdir -p /var/lib/jenkins/plugins/
-chmod -R 0777 /var/lib/jenkins/plugins
+sudo mkdir -p /var/lib/jenkins/plugins/
+sudo chmod -R 0777 /var/lib/jenkins/plugins
 
 /vagrant/jenkins_install_plugins.sh /vagrant/jenkins_plugins.txt
-service jenkins restart
+sudo service jenkins restart
 
 # clear the logs, set folder permissions, restart
-chmod -R 0777 /var/lib/jenkins/plugins
-rm -f /var/log/jenkins/jenkins.log
-cp /vagrant/config.xml /var/lib/jenkins/config.xml
-service jenkins restart
+sudo chmod -R 0777 /var/lib/jenkins/plugins
+sudo rm -f /var/log/jenkins/jenkins.log
+sudo cp /vagrant/config.xml /var/lib/jenkins/config.xml
+sudo service jenkins restart
 echo "INFO: Done installing Jenkins plugins."
 
 
 
 # copy over project setup
 echo "INFO: Copying over Pre-configured Jobs."
-mkdir -p /var/lib/jenkins/jobs/
-cp -r /vagrant/jobs/PipelineDemo /var/lib/jenkins/jobs/
-chmod -R 777 /var/lib/jenkins/jobs/
+sudo mkdir -p /var/lib/jenkins/jobs/
+sudo cp -r /vagrant/jobs/PipelineDemo /var/lib/jenkins/jobs/
+sudo chmod -R 777 /var/lib/jenkins/jobs/
 
 # restart Jenkins
-service jenkins restart
+sudo service jenkins restart
 
 # Autoresize the EC2 root EBS partition, if needed
 if [[ $(df -h | grep 'xvda1') ]]; then
